3c1d1dc9f42ff75b78963daf758b4a6996c91efa
Gunnvalder <kudlukporter@gmail.com>
2024-12-05 19:23:34 -0400

    fixing new problem with laser

--- Assets/Scripts/ShootLaser.cs
+++ Assets/Scripts/ShootLaser.cs
@@ -7,10 +7,18 @@ public class ShootLaser : MonoBehaviour
     public Material material;
     LaserBeam beam;
 
+    private void Start()
+    {
+        beam = new LaserBeam(gameObject.transform.position, gameObject.transform.right, material);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Destroy(GameObject.Find("Laser"));
-        beam = new LaserBeam(gameObject.transform.position, gameObject.transform.right, material);
+        if (beam != null)
+        {
+            Destroy(GameObject.Find("Laser"));
+            beam = new LaserBeam(gameObject.transform.position, gameObject.transform.right, material);
+        }
     }
 }
